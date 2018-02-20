0bc6f17e6448ac7de57efe7837c5e4a0b8ba3241
Soham Adwani <sohamthenerd@gmail.com>
2018-02-20 15:15:22 +0700

    change vimrc to download from this repo, easier to keep track of it

--- server-setup.sh
+++ server-setup.sh
@@ -12,8 +12,7 @@ nvm install 9
 
 sudo apt-get install nginx
 
-wget https://gist.githubusercontent.com/Snazzyham/bad8f06515b493bd766d3e9ac981442a/raw/eb30b18d18b7b02045bc583ddea52ec403faf53c/.tmux.conf
-
-wget https://gist.githubusercontent.com/Snazzyham/89c3e0a185eddfe4b355ef0723174d44/raw/206f10af3fac66c37239ca4dd272138e3db741cb/.vimrc
+wget https://raw.githubusercontent.com/Snazzyham/Server-Setup/master/.tmux.conf
+wget https://raw.githubusercontent.com/Snazzyham/Server-Setup/master/.vimrc
 
 npm install -g serve
